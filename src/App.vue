fc6ec7676be525c7bed470b19a855e1a6be5f22a
Omar Flores <omarefg92@gmail.com>
2017-12-14 12:15:17 -0500

    Fixes url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://omarefg.github.io/platzimusic/dist/logo.png')
     h1 {{ msg }}
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{country.name}}
